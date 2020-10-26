4ef5037198ad413f0548d38c46aff476190b628e
Guilherme Lopes Roque <guilherme.roque@intelbras.com.br>
2020-10-26 17:32:27 -0300

    Pushing to git

---
+++ forca.c
@@ -0,0 +1,353 @@
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+#include "forca.h"
+
+void desenha(int escolha){
+	switch(escolha){
+		case 0:
+printf("          `;,;.;,;.;.'						\n");
+printf("              ..:;:;::;: 					\n");
+printf("        ..--''' '' ' ' '''--.  				\n");
+printf("      /' .   .'        '.   .`\\			\n");
+printf("     | /    /            \\   '.|			\n");
+printf("     | |   :             :    :|			\n");
+printf("   .'| |   :             :    :|			\n");
+printf(" ,: /\\ \\.._\\ __..===..__/_../ /`.		\n");
+printf("|'' |  :.|  `'          `'  |.'  ::.		\n");
+printf("|   |  ''|    :'';          | ,  `''\\		\n");
+printf("|.:  \\/  | /'-.`'   ':'.-'\\ |  \\,   |	\n");
+printf("| '  /  /  | / |...   | \\ |  |  |';'|		\n");
+printf(" \\ _ |:.|  |_\\_|`.'   |_/_|  |.:| _ |		\n");
+printf("/,.,.|' \\__       . .      __/ '|.,.,\\	\n");
+printf("     | ':`.`----._____.---'.'   |			\n");
+printf("l42   \\   `: ""-------'""'|	|   		\n");
+printf("       ',-,-',             .'-=,=,     		\n");		
+						break;
+		
+		case 1:
+printf("          .  .					\n");
+printf("          |\\_|\\				\n");
+printf("          | a_a\\				\n");
+printf("          | | ']				\n");
+printf("      ____| '-\\___				\n");
+printf("     /.----.___.-'\\			\n");
+printf("    //        _    \\			\n");
+printf("   //   .-. (~v~) /|			\n");
+printf("  |'|  /\\:  .--  / \\			\n");
+printf(" // |-/  \\_/____/\\/~|			\n");
+printf("|/  \\ |  []_|_|_] \\ |			\n");
+printf("| \\  | \\ |___   _\\ ]_}		\n");
+printf("| |  '-' /   '.'  |				\n");
+printf("| |     /    /|:  | 			\n");
+printf("| |     |   / |:  /\\			\n");
+printf("| |     /  /  |  /  \\			\n");
+printf("| |    |  /  /  |    \\			\n");
+printf("\\ |    |/\\/  |/|/\\    \\		\n");
+printf(" \\|\\ |\\|  |  | / /\\/\\__\\	\n");
+printf("  \\ \\| | /   | |__			\n");
+printf("snd    / |   |____)				\n");
+printf("       |_/						\n");
+					break;
+			
+		case 2:
+printf("      .--.   _,								\n");
+printf("  .--;    \\ /(_							\n");	
+printf(" /    '.   |   '-._    . ' .				\n");
+printf("|       \\  \\    ,-.)  -= * =-				\n");
+printf(" \\ /\\_   '. \\((` .(    '/. '				\n");
+printf("  )\\ /     \\ )\\  _/   _/					\n");
+printf(" /  \\    .-'   '--. /_\\					\n");
+printf("|    \\_.' ,        \\/||					\n");
+printf("\\     \\_.-';,_) _)'\\ \\||				\n");
+printf(" '.       /`\\   (   '._/					\n");
+printf("   `\\   .;  |  . '.						\n");
+printf("jgs  ).'  )/|      \\						\n");
+printf("     `    ` |  \\|   |						\n");
+printf("             \\  |   |						\n");
+printf("              '.|   |						\n");
+printf("                 \\  '\\__					\n");
+printf("                  `-._  '. _				\n");
+printf("                     \\`;-.` `._			\n");
+printf("                      \\ \\ `'-._\\			\n");
+printf("                       \\ |					\n");
+printf("                        \\ )				\n");
+printf("                         \\_\\				\n");
+					break;
+			
+		case 3:
+printf("          _.-*''''*-._				\n");
+printf("       .-'            '-.			\n");
+printf("     ,'                  ',			\n");
+printf("   .'      _.-.--.-._      ',		\n");
+printf("  /     .-'.-'    '-.'-.     \\		\n");
+printf(" /     /  /''-.  .-''\\  \\     \\	\n");
+printf(":     :  :     ;:     ;  ;     ;	\n");
+printf(";     :  ; *   !! *   :  ;     :	\n");
+printf(";      ; :   .'  '.   ; :      :	\n");
+printf(":       \\ \\-'      '-/ /       ;	\n");
+printf(" \\       '.'-_    _-'.'       /	\n");
+printf("  \\        '*-'-+'-*'        /		\n");
+printf("   '.          /|          .'		\n");
+printf("     *,       / |        ,*			\n");
+printf("     / '-_            _-'  \\		\n");
+printf("    /     '*-.____.-*'      \\		\n");
+printf("   /            |            \\		\n");
+printf("  :    :        |        ;    ;		\n");
+printf("  |.--.;        |        :.--.|		\n");
+printf("  (   ()        |        ()   )		\n");
+printf("   '--^_        |        _^--'		\n");
+printf("      | ''*--.._I_..--*'' |			\n");
+printf("      | __..._  | _..._   |			\n");
+printf("     .''      `'''     ''''.		\n");
+printf("     '''''''''''''''''''''''		\n");
+printf("       K E N N Y by Matzec			\n");
+						break;
+			
+		case 4:
+
+printf("           o										\n");
+printf("           |										\n");
+printf("         ,'~'.										\n");
+printf("        /     \\									\n");
+printf("       |   ____|_									\n");
+printf("       |  '___,,_'         .----------------.		\n");
+printf("       |  ||(o |o)|       ( KILL ALL HUMANS! )		\n");
+printf("       |   -------         ,----------------'		\n");
+printf("       |  _____|         -'							\n");
+printf("       \\  '####,									\n");
+printf("        -------										\n");
+printf("      /________\\									\n");
+printf("    (  )        |)									\n");
+printf("    '_ ' ,------|\\         _						\n");
+printf("   /_ /  |      |_\\        ||						\n");
+printf("  /_ /|  |     o| _\\      _|| 						\n");
+printf(" /_ / |  |      |\\ _\\____//' |					\n");
+printf("(  (  |  |      | (_,_,_,____/						\n");
+printf(" \\ _\\ |   ------|        							\n");
+printf("  \\ _\\|_________|									\n");
+printf("   \\ _\\ \\__\\__\\								\n");
+printf("   |__| |__||__|									\n");
+printf("||/__/  |__||__|									\n");
+printf("        |__||__|									\n");
+printf("        |__||__|									\n");
+printf("        /__)/__)									\n");
+printf("       /__//__/										\n");
+printf("      /__//__/										\n");
+printf("     /__//__/.										\n");
+printf("   .'    '.   '.									\n");
+printf("  (_kOs____)____)									\n");
+							break;
+			
+		case 5:
+printf("\n\n\n\n");
+printf("            ( _)                ( _)					\n");
+printf("           / / \\              / /\\_\\_				\n");
+printf("          / /   \\            / / | \\ \\				\n");
+printf("         / /     \\          / /  |\\ \\ \\				\n");
+printf("        /  /   ,  \\ ,       / /   /|  \\ \\			\n");
+printf("       /  /    |\\_ /|      / /   / \\   \\_\\			\n");
+printf("      /  /  |\\/ _ '_| \\   / /   /   \\    \\			\n");
+printf("     |  /   |/  0 \\0\\    / |    |    \\    \\			\n");
+printf("     |    |\\|      \\_\\_ /  /    |     \\    \\		\n");
+printf("     |  | |/    \\.\\ o\\o)  /      \\     |    \\		\n");
+printf("     \\    |     /\\`v-v  /        |    |     \\		\n");
+printf("      | \\/    /_| \\_|  /         |    | \\    \\		\n");
+printf("      | |    /__/_ `-` /   _____  |    |  \\    \\		\n");
+printf("      \\|    [__]  \\_/  |_________  \\   |   \\    ()	\n");
+printf("       /    [___] (    \\         \\  |\\ |   |   //	\n");
+printf("      |    [___]                  |\\| \\|   /  |/		\n");
+printf("     /|    [____]                  \\  |/\\ / / ||		\n");
+printf("snd (  \\   [____ /     ) _\\      \\  \\    \\| | ||	\n");
+printf("     \\  \\  [_____|    / /     __/    \\   / / //		\n");
+printf("     |   \\ [_____/   / /        \\    |   \\/ //		\n");
+printf("     |   /  '----|   /=\\____   _/    |   / //			\n");
+printf("  __ /  /        |  /   ___/  _/\\    \\  | ||			\n");
+printf(" (/-(/-\\)       /   \\  (/\\/\\)/  |    /  | /			\n");
+printf("               (/\\/\\)           /   /   //			\n");
+printf("                      _________/   /    /				\n");
+printf("                     \\____________/    (				\n");
+printf("					       .--.   _,  					\n");		
+							break;
+			
+		case 6:
+printf("       _____,    _..-=-=-=-=-====--,		\n");
+printf("     _.'a   /  .-',___,..=--=--==-'`		\n");
+printf("    ( _     \\ /  //___/-=---=----'			\n");
+printf("     ` `\\    /  //---/--==----=-'			\n");
+printf("  ,-.    | / \\_//-_.'==-==---='			\n");
+printf(" (.-.`\\  | |'../-'=-=-=-=--'				\n");
+printf("  (' `\\`\\| //_|-\\.`;-~````~,        _	\n");
+printf("       \\ | \\_,_,_\\.'        \\     .'_`\\\n");
+printf("        `\\            ,    , \\    || `\\	\n");
+printf("          \\    /   _.--\\    \\ '._.'/  / |\n");
+printf("          /  /`---'   \\ \\   |`'---'   \\/	\n");
+printf("         / /'          \\ ;-. \\			\n");
+printf("jgs   __/ /           __) \\ ) `|			\n");
+printf("    ((='--;)         (,___/(,_/				\n");
+							break;
+	}
+}
+
+int teste_letra(char letra){
+	if (((letra >= 'a') && (letra <= 'z')) || ((letra >= 'A') && (letra <= 'Z')))
+		return 1;
+	else
+		return 0;
+}
+
+void abertura() {
+	system("clear");
+    printf("    ****************\n");
+    printf("     Jogo de Forca    \n");
+    printf("    ****************  \n\n");
+}
+
+
+void chuta(char chutes[],int cont,int erros){
+	while(1){
+		printf("Erros: %d  Numero de Chutes:  %d   \n--  Chute uma letra:  ",erros,cont);
+		scanf(" %c",&chutes[cont]);
+		printf("\n");
+		if (teste_letra(chutes[cont]))
+			break;
+		else
+			printf("Erro - character digitado nao e uma letra\n\n");
+	}
+			
+}
+
+int jachutou(char chutes[],char palavra[],int cont,int erros){
+	for (int x=0;x<(strlen(chutes) -1 );x++){
+		if (chutes[x]== chutes[cont]){
+			printf("Ja foi chutado\n");
+			return 1;
+		}
+	}
+	return 0;
+}
+
+void acertou(char palavra[],char chutes[],int cont,int (*erros),int (*acertos)){
+	int achou=0;
+	for(int x=0;x<strlen(palavra);x++)
+		if ((chutes[cont] == palavra[x])||((chutes[cont]-32)== palavra[x])||((chutes[cont]+32)== palavra[x])){
+			(*acertos)++;
+			achou =1;
+		}
+	if (!achou)
+		(*erros)++;		
+	
+}
+
+void desenha_forca(char palavra[],char chutes[],int erros){
+	int achou;
+	int x,y;
+	system("clear");
+	printf("  _______       \n");
+	printf(" |/      |      \n");
+	printf(" |      %c%c%c  \n", (erros>=1?'(':' '), 
+		(erros>=1?'_':' '), (erros>=1?')':' '));
+	printf(" |      %c%c%c  \n", (erros>=3?'\\':' '), 
+		(erros>=2?'|':' '), (erros>=3?'/': ' '));
+	printf(" |       %c     \n", (erros>=2?'|':' '));
+	printf(" |      %c %c   \n", (erros>=4?'/':' '), 
+		(erros>=4?'\\':' '));
+	printf(" |              \n");
+	printf("_|___           \n");
+	printf("\n\n");
+	for(x=0;x<strlen(palavra);x++){	
+		achou=0;
+		for (y=0;y<strlen(chutes);y++)
+			if((chutes[y] == palavra[x])||((chutes[y]-32)== palavra[x])||((chutes[y]+32)== palavra[x])){	
+				achou=1;
+				break;
+			}
+			if (achou)
+				printf("%c ",palavra[x]);
+			else
+		if (teste_letra(palavra[x]))
+			printf(" _ ");
+		else
+			printf(" | ");
+	}
+	printf("\n");
+}
+void escolhe_palavra(char palavra[],int (*escolha)){
+	srand(time(0));	
+	(*escolha) = rand() %7;
+	switch ((*escolha)){
+		case 0: 
+		sprintf(palavra,"Bulbasaur");
+		printf("Dica: Pokemon\n");		
+		break;
+		
+		case 1:
+		sprintf(palavra,"Batman");
+		printf("Dica: Heroi\n");
+		break;
+			
+		case 2:		
+		sprintf(palavra,"Tinkerbell");
+		printf("Dica: Disney\n");
+		break;
+			
+		case 3:
+		sprintf(palavra,"Kenny");
+		printf("Dica: South Park\n");
+		break;
+			
+		case 4:
+		sprintf(palavra,"Bender");
+		printf("Dica: Futurama\n");
+		break;
+		
+		case 5:
+		sprintf(palavra,"Dragao");
+		printf("Dica: Monstro\n");
+		break;
+			
+		case 6:
+		sprintf(palavra,"Grifo");
+		printf("Dica: Harry Potter e o Prisioneiro de Azkaban\n\n");
+		break;
+	}	
+
+
+	for (int cont = 0; cont<strlen(palavra);cont++)
+		if (teste_letra(palavra[cont]))
+			printf(" _ ");
+		else
+			printf(" | ");
+	printf("\n\n");
+	
+}
+
+
+
+int main() {
+	int acertos =0;
+	char palavra[20];
+	char chutes[20]={""};
+	int cont = 0;
+	int erros = 0;
+	int escolha;
+	abertura();
+	escolhe_palavra(palavra,&escolha);
+	do{
+		chuta(chutes,cont,erros);
+		if (!jachutou(chutes,palavra,cont,erros)){
+			acertou(palavra,chutes,cont,&erros,&acertos);
+			desenha_forca(palavra,chutes,erros);
+		}
+		cont++;
+		}while ((erros < 5 ) && ((acertos) < (strlen(palavra))));
+	
+	if(!(acertos < strlen(palavra)))
+		desenha(escolha);
+	else{
+		printf("\n\n");
+		printf("         --------ENFORCOU---------     \n\n");
+	}
+	return 0; 
+}
---
+++ forca.h
@@ -0,0 +1,8 @@
+void desenha();
+void abertura();
+void chuta();
+int jachutou();
+void acertou();
+void desenha_forca();
+void escolhe_palavra();
+int teste_letra(char letra);
